1fa4af1fb8db4165db4d4ccc88108a742fb961b3
Rudra <khandelwalrudra1305@gmail.com>
2025-04-26 17:36:38 +0530

    updated public key

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,4 +4,4 @@ import os
 load_dotenv()
 
 SUPABASE_URL = os.getenv("https://tsruvplxeloculjwhpmb.supabase.co")
-SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzcnV2cGx4ZWxvY3VsandocG1iIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTY1OTY2NCwiZXhwIjoyMDYxMjM1NjY0fQ.nubppa8TmDxSaxW7j1Wj96au0S-fZ5hGaOX8uJHt-m0")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzcnV2cGx4ZWxvY3VsandocG1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NTk2NjQsImV4cCI6MjA2MTIzNTY2NH0.gfvqota3DYSb6CxzXyIZmfvO7bminWjNjDSJTWW0ftk")
